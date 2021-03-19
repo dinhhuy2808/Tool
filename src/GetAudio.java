982cda1ebbb85b72f85e563b64d13ea15890bd1e
huyttran <huytran.jp@interspace.vn>
2021-03-19 17:35:36 +0700

    c

--- src/GetAudio.java
+++ src/GetAudio.java
@@ -3,60 +3,70 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.util.StringUtil;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class GetAudio {
-	public static void main(String[] args) throws IOException {
-		// Log.d(LOG_TAG, "url.openStream()");
-
-		Workbook  workbook = null;
-		Row row = null;
-		try {
-			InputStream inputExcelStream = new FileInputStream(new File(
-					"Tu-Dien-Website.xlsx"));
-
-			workbook = new XSSFWorkbook(inputExcelStream);
-			List<String> files = getAllFilesNameInFolder("C:\\GURKHA_DEV\\eLearning\\audio\\audio");
-			Sheet sheet = workbook.getSheetAt(0);
-			int lastRow = sheet.getLastRowNum();
-			for (int i = /* totalWords+1 */1; i <= 10; i++) {
-				row = sheet.getRow(i);
-				String hantu = row.getCell(1) == null ? "" : row.getCell(1).getStringCellValue().replace("\'", "\\'");
-				if (!files.contains(hantu.trim()+".mp3")) {
-					URL url = new URL("https://vtudien.com/doc/trung/"+hantu+".mp3");
-					InputStream inputStream = url.openStream();
-					FileOutputStream fileOutputStream = new FileOutputStream("hantu.mp3");
-
-					int c;
-
-					while ((c = inputStream.read()) != -1) {
-						fileOutputStream.write(c);
-						c++;
-					}
-				}
-			}
-			// }
-		} catch (IOException | NullPointerException e) {
-			// TODO Auto-generated catch block
-			System.out.println(row.getRowNum());
-			System.out.println(e.getMessage());
-		}
-
-	}
+	public static void main(String[] args) throws IOException {reName();}
 	
 	public static List<String> getAllFilesNameInFolder(String path) {
 		File[] files = new File(path).listFiles();
 		return Arrays.asList(files).stream().map(file -> file.getName()).collect(Collectors.toList());
+	}
+	
+	public static void reName() throws UnsupportedEncodingException, IOException {
+		boolean strictMode = true;
+		String textChinese = new String(Files.readAllBytes(Paths.get("D:\\Tax\\eLearning\\src\\shared\\components\\pinyin-chart\\chinese-pinyin-chart.html")),
+				"UTF-8");
+		String unescapedStringChinese = org.jsoup.parser.Parser.unescapeEntities(textChinese, strictMode);
+		Document docChinese = Jsoup.parse(unescapedStringChinese, "UTF-8");
+		Elements elementsChinese = docChinese.getElementsByClass("click");
+		Map<String, List<String>> madarinMap = new HashMap<String, List<String>>();
+		for(Element element : elementsChinese) {
+			String[] s = element.text().split(" ");
+			madarinMap.put(s[0], Arrays.asList(s[1], s[2], s[3], s[4]));
+		}
+		
+		System.out.println(madarinMap);
+		String filePath =  "C:\\TaxProject\\upload\\public\\audios\\PinyinChart";
+		List<File> files = Arrays.asList(new File(filePath).listFiles());
+		for(File file : files) {
+			String fileName = file.getName().split("\\.")[0];
+			String validFileName = (fileName.endsWith("1") ||fileName.endsWith("2") ||
+					fileName.endsWith("3") ||fileName.endsWith("4") )?fileName:"";
+			if (!validFileName.isEmpty()) {
+				int index = Integer.parseInt(validFileName.substring(validFileName.length()-1));
+				String key = validFileName.substring(0, validFileName.length()-1);
+				try {
+					System.out.println(index + " " + key + " " + madarinMap.get(key).get(index-1));
+					if (!file.renameTo(new File(filePath + "\\"+madarinMap.get(key).get(index-1)+".mp3"))) {
+						throw new NullPointerException();
+					}
+				} catch (NullPointerException e) {
+					System.out.println("--------------" + validFileName);
+				}
+				
+			}
+		}
 	}
 }
--- src/YoyoChinesePinyinChart.java
+++ src/YoyoChinesePinyinChart.java
@@ -3,77 +3,55 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.jsoup.Jsoup;
-import org.jsoup.nodes.Attributes;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.jsoup.nodes.Node;
-import org.jsoup.nodes.Document.OutputSettings;
 import org.jsoup.select.Elements;
 
-import com.google.gson.Gson;
-
 public class YoyoChinesePinyinChart {
-	
 	public static void main(String[] args) throws IOException {
-		String text = new String(Files.readAllBytes(Paths.get("D://Tax//Yoyo-Chinese-Pinyin-Chart.htm")),
-				"ISO-8859-1");
-		System.out.println(text);
-		Document doc = Jsoup.parse(text, "ISO-8859-1");
-		Elements tdElements = doc.getElementsByTag("td");
-		String audioTemp = "<span class=\"tooltiptext\"><div> <audio id=\"player\" src=\"%s\"/> <div> <a onclick=\"document.getElementById('player').play()\"> <i class='fa fa-volume-up'/>%s</a> </div> </div> <div> <audio id=\"player\" src=\"%s\"/> <div> <a onclick=\"document.getElementById('player').play()\"> <i class='fa fa-volume-up'/>%s</a> </div> </div> <div> <audio id=\"player\" src=\"%s\"/> <div> <a onclick=\"document.getElementById('player').play()\"> <i class='fa fa-volume-up'/>%s</a> </div> </div> <div> <audio id=\"player\" src=\"%s\"/> <div> <a onclick=\"document.getElementById('player').play()\"> <i class='fa fa-volume-up'/>%s</a> </div> </div></span>";
-		String audioUrlTemp = "http://103.92.29.71:8081/audios/Pinyin/%s.mp3";
-		for(Element td : tdElements) {
-			
-			if (td.attr("width").equals("68")) {
-				td.removeAttr("style");
-				td.addClass("head");
-			} else if (td.attr("width").equals("52")) {
-				td.removeAttr("style");
-				td.addClass("cell");
-				String textInner = td.text();
-				if (textInner.matches("[a-zA-Z]+")) {
-					td.attr("id", textInner);
-//					System.out.println(textInner);
-					td.attr("onClick", String.format("openTootip('%s', this)", textInner));
-					String audioUrl = String.format(audioUrlTemp, textInner.trim());
-					String audiohtml = String.format(audioTemp, String.format(audioUrlTemp, textInner.trim())+"1", " " + textInner +"1 "
-							, String.format(audioUrlTemp, textInner.trim())+"2", " " + textInner +"2 "
-							, String.format(audioUrlTemp, textInner.trim())+"3", " " + textInner +"3 "
-							, String.format(audioUrlTemp, textInner.trim())+"4", " " + textInner +"4 ");
-					td.html(td.html()+audiohtml);
-				}
-				
-				
-			}
+		String text = new String(Files.readAllBytes(Paths.get("D:\\Tax\\eLearning\\src\\shared\\components\\pinyin-chart\\pinyin-chart.component-copy.html")),
+				"UTF-8");
+		boolean strictMode = true;
+		String textChinese = new String(Files.readAllBytes(Paths.get("D:\\Tax\\eLearning\\src\\shared\\components\\pinyin-chart\\chinese-pinyin-chart.html")),
+				"UTF-8");
+		String unescapedStringChinese = org.jsoup.parser.Parser.unescapeEntities(textChinese, strictMode);
+		Document docChinese = Jsoup.parse(unescapedStringChinese, "UTF-8");
+		Elements elementsChinese = docChinese.getElementsByClass("click");
+		Map<String, List<String>> madarinMap = new HashMap<String, List<String>>();
+		for(Element element : elementsChinese) {
+			String[] s = element.text().split(" ");
+			madarinMap.put(s[0], Arrays.asList(s[1], s[2], s[3], s[4]));
 		}
-		Element firstTableTr = doc.selectFirst("div.WordSection1 > table > tbody > tr");
-		firstTableTr.attr("style", firstTableTr.attr("style")+";background: #00bfd8;");
-		Element firstTableTrTd = firstTableTr.getElementsByTag("td").get(0);
-		firstTableTrTd.attr("style", firstTableTrTd.attr("style")+";background: #FFF4DF;");
+//		System.out.println(madarinMap);
 		
-		Element firstTableTr2 = doc.selectFirst("div.WordSection2 > table > tbody > tr");
-		firstTableTr2.attr("style", firstTableTr2.attr("style")+";background: #00bfd8;");
-		Element firstTableTrTd2 = firstTableTr2.getElementsByTag("td").get(0);
-		firstTableTrTd2.attr("style", firstTableTrTd2.attr("style")+";background: #FFF4DF;");
+		for (String key : madarinMap.keySet()) {
+			List<String> value = madarinMap.get(key);
+			text = text.replace(key.trim()+"1", value.get(0))
+			.replace(key.trim()+"2", value.get(1))
+			.replace(key.trim()+"3", value.get(2))
+			.replace(key.trim()+"4", value.get(3));
+		}
 		
-		Element firstTableTr3 = doc.selectFirst("div.WordSection3 > table > tbody > tr");
-		firstTableTr3.attr("style", firstTableTr3.attr("style")+";background: #00bfd8;");
-		Element firstTableTrTd3 = firstTableTr3.getElementsByTag("td").get(0);
-		firstTableTrTd3.attr("style", firstTableTrTd3.attr("style")+";background: #FFF4DF;");
-		try {
+		String unescapedString = org.jsoup.parser.Parser.unescapeEntities(text, strictMode);
+		Document doc = Jsoup.parse(unescapedString, "UTF-8");
+		Elements elements = doc.getElementsByClass("example");
+		System.out.println(text);
+		/*try {
 			Writer out = new BufferedWriter(new OutputStreamWriter(
-				    new FileOutputStream("D://Tax//Yoyo-Chinese-Pinyin-Chart - Copy.htm"), "ISO-8859-1"));
+				    new FileOutputStream("D:\\Tax\\eLearning\\src\\shared\\components\\pinyin-chart\\pinyin-chart.component-copy.html"), "UTF-8"));
 			  out.write(doc.html());
 
 			  out.close();
 		} catch (IOException e2) {
 			e2.printStackTrace();
-		}
+		}*/
 	}
 }
